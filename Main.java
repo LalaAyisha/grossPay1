e91e6a5f5b1424ce03d2b17a2eb44aac9e2da386
LalaAyisha <115223069+LalaAyisha@users.noreply.github.com>
2022-12-28 01:03:49 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double x = 20.0;
+        double y = 15.0;
+        double z = 30.0;
+        System.out.println(" Grosspay is " + x * y * z);
+    }
+}
